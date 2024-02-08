e8c26a656c5736191c1bf753cae97a5e66feaec2
markdohann <159459629+markdohann@users.noreply.github.com>
2024-02-08 21:10:38 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-    x.classList.toggle("menu");
-  }
+function openNav(){
+    document.getElementById("myNav").style.width = "75%";  
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width = "0%";
+}
+
+
+
+
